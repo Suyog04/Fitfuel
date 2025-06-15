19bd6cac5b924256cb5491fd849fe2c57d56ca12
Suyog Bista <bista.suyog5@gmail.com>
2025-06-15 21:51:32 +0545

    Login and register improvement

---
+++ Fitfuel/Migrations/20250612171533_AddTimestampToCalorieEntry.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fitfuel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampToCalorieEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Calories",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Carbs",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fats",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Fiber",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Meal",
+                table: "CalorieEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Protein",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "WeightInGrams",
+                table: "CalorieEntries",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Carbs",
+                table: "CalorieEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Fats",
+                table: "CalorieEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Fiber",
+                table: "CalorieEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Meal",
+                table: "CalorieEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Protein",
+                table: "CalorieEntries");
+
+            migrationBuilder.DropColumn(
+                name: "WeightInGrams",
+                table: "CalorieEntries");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Calories",
+                table: "CalorieEntries",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
