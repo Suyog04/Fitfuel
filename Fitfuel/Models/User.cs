ddc795ad37f7b4b2d46415deb87271deb5deaabe
Suyog Bista <bista.suyog5@gmail.com>
2025-06-30 21:40:38 +0545

    updated user model

--- Fitfuel/Models/User.cs
+++ Fitfuel/Models/User.cs
@@ -1,13 +1,33 @@
-
 using FitFuel.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 public class User
 {
+    [Key]
     public Guid UserId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; set; } // Add this
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public virtual ICollection<CalorieEntry> CalorieEntries { get; set; } = new List<CalorieEntry>();
 
+    [Required, StringLength(100)]
+    public string Name { get; set; }
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // ✅ New fields
+    [Range(50, 300)] // in cm
+    public double HeightCm { get; set; }
+
+    [Range(20, 300)] // in kg
+    public double WeightKg { get; set; }
+
+    [Range(5, 120)]
+    public int Age { get; set; }
+
+    public virtual ICollection<CalorieEntry> CalorieEntries { get; set; } = new List<CalorieEntry>();
 }
